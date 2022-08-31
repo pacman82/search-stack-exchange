67fde2b4c59c2bb7ee3a52ab85ffe665747c0e7d
Markus Klein <markus-klein@live.de>
2022-08-31 20:34:15 +0200

    Count questions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use quick_xml::{Reader, events::Event};
+use atoi::FromRadix10;
+use quick_xml::{events::{Event, attributes::{Attribute, AttrError}}, Reader};
 use std::{
     fs::File,
     io::{self, BufReader},
@@ -13,7 +14,7 @@ pub enum Error {
     #[error("Invalid xml format: {0}")]
     InvalidXMLFormat(String),
     #[error("XML input is malformed")]
-    MalformedXML(String)
+    MalformedXML(String),
 }
 
 /// Parses Stack Exchange Post XMLs
@@ -36,24 +37,90 @@ impl PostReader {
         // Read declaration: E.g.: <?xml version="1.0" encoding="utf-8"?>
         let event = Self::extract_event(&mut xml_reader, &mut buf)?;
         if !matches!(event, Event::Decl(_)) {
-            return Err(Error::InvalidXMLFormat("Expected XML Declaration".to_owned()))
+            return Err(Error::InvalidXMLFormat(
+                "Expected XML Declaration".to_owned(),
+            ));
         }
-        // Read start rows event. E.g. <posts>
+        // Read start of rows collection; e.g. <posts>
         let event = Self::extract_event(&mut xml_reader, &mut buf)?;
         if !matches!(event, Event::Start(_)) {
-            return Err(Error::InvalidXMLFormat("Expected XML Start Rows".to_owned()))
+            return Err(Error::InvalidXMLFormat(
+                "Expected XML Start Rows".to_owned(),
+            ));
         }
         Ok(Self { buf, xml_reader })
     }
 
-    fn extract_event<'a>(xml_reader: &mut Reader<BufReader<File>>, buf: &'a mut Vec<u8>) -> Result<Event<'a>, Error> {
+    fn extract_event<'a>(
+        xml_reader: &mut Reader<BufReader<File>>,
+        buf: &'a mut Vec<u8>,
+    ) -> Result<Event<'a>, Error> {
         buf.clear();
-        let result = xml_reader
-            .read_event_into(buf);
+        let result = xml_reader.read_event_into(buf);
         match result {
             Ok(event) => Ok(event),
             Err(quick_xml::Error::Io(cause)) => Err(Error::ReadXmlFile(cause)),
             Err(error) => Err(Error::MalformedXML(error.to_string())),
         }
+    }
+
+    pub fn next_post(&mut self) -> Result<Option<Post>, Error> {
+        let event = Self::extract_event(&mut self.xml_reader, &mut self.buf)?;
+        match event {
+            Event::Empty(bytes) => {
+                let name = bytes.name();
+                if name.as_ref() == b"row" {
+                    let post = Post::from_attributes(bytes.attributes())?;
+                    Ok(Some(post))
+                } else {
+                    Err(Error::InvalidXMLFormat(format!(
+                        "Unexpected tagname in row: {}",
+                        String::from_utf8_lossy(name.as_ref())
+                    )))
+                }
+            }
+            Event::End(_) => Ok(None),
+            _ => Err(Error::InvalidXMLFormat(
+                "Unexpected tag. Expected row.".to_owned(),
+            )),
+        }
+    }
+}
+
+pub enum Post
+{
+    Question { id: u64},
+    Other,
+}
+
+impl Post {
+    fn from_attributes<'a>(attributes: impl Iterator<Item = Result<Attribute<'a>, AttrError>>) -> Result<Self, Error> {
+        let mut id = None;
+        let mut post_type_id = None;
+
+        for attr in attributes {
+            let attr = attr?;
+            match attr.key.into_inner() {
+                b"Id" => id = Some(attr.value),
+                b"PostTypeId" => post_type_id = Some(attr.value),
+                _ => (),
+            }
+        }
+        let post_type_id = post_type_id.ok_or_else(|| Error::InvalidXMLFormat("Missing post_type_id in Post".to_owned()))?;
+        let post = match post_type_id.as_ref() {
+            b"1" => {
+                let id = id.ok_or_else(|| Error::InvalidXMLFormat("Missing id in Post".to_owned()))?;
+                let (id, _) = u64::from_radix_10(&id);
+                Post::Question { id }
+            }
+            _ => Post::Other,
+        };
+        Ok(post)
+    }
+}
+
+impl From<AttrError> for Error {
+    fn from(source: AttrError) -> Self {
+        Error::MalformedXML(source.to_string())
     }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,34 +1,15 @@
-use search_stack_exchange::PostReader;
-use quick_xml::{events::Event};
+use search_stack_exchange::{PostReader, Post};
 
 #[test]
 fn find_similar_question() {
     // Parse Post.xml from 3dprinting stackexchange dump. We choose the 3d printing dump, because it
     // is one of the smaller ones.
     let mut reader = PostReader::new("./tests/3dprinting.Posts.xml").unwrap();
-    // Self start row event
-    let row_event = reader.xml_reader.read_event_into(&mut reader.buf).unwrap();
-    // let row_event = reader.read_event_into(&mut buf).unwrap();
-    match row_event {
-        Event::Empty(e) => {
-            assert_eq!(e.name().as_ref(), b"row");
-            let mut id = None;
-            let mut post_type_id = None;
-            for attr in e.attributes() {
-                println!("{attr:?}");
-                let attr = attr.unwrap();
-                match attr.key.into_inner() {
-                    b"Id" => id = Some(attr.value),
-                    b"PostTypeId" => post_type_id = Some(attr.value),
-                    _ => (),
-                }
-            }
-            assert_eq!(id.as_deref(), Some(b"1".as_slice()));
-            assert_eq!(post_type_id.as_deref(), Some(b"1".as_slice()));
+    let mut num_questions = 0;
+    while let Some(post) = reader.next_post().unwrap() {
+        if matches!(post, Post::Question { .. }) {
+            num_questions += 1;
         }
-        // Event::Text(e) => {
-
-        // }
-        any => panic!("Expected empty event. Found {any:?}"),
     }
+    assert_eq!(4800, num_questions);
 }
