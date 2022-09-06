8182098b907d49ed6017a60c8b715d72324ac3a4
Markus Klein <markus-klein@live.de>
2022-09-06 14:09:00 +0200

    faster test execution

--- tests/integration.rs
+++ tests/integration.rs
@@ -11,7 +11,7 @@ lazy_static! {
 }
 
 /// Path to full 3D printing posts.xml
-const THREE_D_PRINTING_POSTS: &str = "./tests/3dprinting.Posts.xml";
+// const THREE_D_PRINTING_POSTS: &str = "./tests/3dprinting.Posts.xml";
 /// Smaller sample for quicker tests
 const SMALL_POSTS: &str = "./tests/small-posts.xml";
 
@@ -19,14 +19,14 @@ const SMALL_POSTS: &str = "./tests/small-posts.xml";
 fn count_all_questions_in_3d_printing() {
     // Parse Post.xml from 3dprinting stackexchange dump. We choose the 3d printing dump, because it
     // is one of the smaller ones.
-    let mut reader = PostReader::new(THREE_D_PRINTING_POSTS).unwrap();
+    let mut reader = PostReader::new(SMALL_POSTS).unwrap();
     let mut num_questions = 0;
     while let Some(post) = reader.next_post().unwrap() {
         if matches!(post, Post::Question { .. }) {
             num_questions += 1;
         }
     }
-    assert_eq!(4800, num_questions);
+    assert_eq!(3, num_questions);
 }
 
 #[tokio::test]
